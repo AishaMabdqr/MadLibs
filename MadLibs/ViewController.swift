bcc34774744b47d526e4b2c609b5b038227d7251
Aisha Abdq <Ashmq22@gmail.com>
2021-12-19 15:48:56 +0300

    Initial Commit

---
+++ MadLibs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MadLibs
+//
+//  Created by A Ab. on 15/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
